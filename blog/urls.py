b8d9c23ece1bf7c1981f74597ff4b97aa523e7af
Parth Gupta <51705666+parthgpta@users.noreply.github.com>
2019-10-12 12:48:25 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    parth('post/remove/<int:pk>/', views.post_del , name='post_del'),
 ]

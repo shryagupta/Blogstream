cd11d27db287c92e7acf216f1fc0ce6daabed463
Shreya Gupta <51705629+shryagupta@users.noreply.github.com>
2019-10-14 23:12:54 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('profile/', views.profile , name = 'profile_view'),
 ]

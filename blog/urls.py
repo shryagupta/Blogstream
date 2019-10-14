eee7d93c837e649722ecb8e40cc176ccccbf5c17
Shreya Gupta <51705629+shryagupta@users.noreply.github.com>
2019-10-14 23:19:01 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('profile/', views.profile , name = 'profile_view'),
+    path('post/del/<int:pk>' , views.post_del , name = 'post_del'),
 ]
